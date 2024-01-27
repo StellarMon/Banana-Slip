a2ca50531c711ddd04265d5d1cf2049e2eea31b3
Logan <loganhobson31@gmail.com>
2024-01-27 15:18:02 -0600

    More scripts for my Library

---
+++ Assets/Scripts/SolarStudios/StateMachineState.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace SolarStudios
+{
+    public class StateMachineState : MonoBehaviour, IState
+    {
+        public void Enter(StateMachine stateMachine) //Runs when we enter the state
+        {
+           
+        }
+        public void Run() //Runs every frame
+        {
+
+        }
+        public void Exit() //Runs when we exit
+        {
+
+        }
+    }
+
+}
