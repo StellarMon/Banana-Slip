c57d05fb83d2274fd3b21b5c67e8c7795a541166
Will Whiite <will.white.v1@gmail.com>
2024-01-28 10:37:50 -0600

    npc health and health reduction

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
+    // npc health
+    public int health;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health < 1)
+        {
+            Destroy(gameObject, 4f);
+        }
+
     }
+
+
 }
